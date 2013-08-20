6a09c1f95643239cbd827670cdea5c00968c1ff9
Wil Reichert <wil.reichert@kt.com>
2013-08-20 14:10:58 +0900

    register_member name update

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,6 +11,9 @@ class Chef::Recipe
   include KTCUtils
 end
 
+d = get_openstack_service_template(get_interface_address("management"), "11211")
+register_member("memcached", d)
+
 node.default["memcached"]["listen"] = get_interface_address "management"
 
 include_recipe "openstack-object-storage::memcached"
