bf450fd506f34a827915c9e2747afa3191d049ad
Robert <taeil.choi@kt.com>
2013-11-11 15:18:41 +0900

    add process monitoring

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,8 @@
 #
-
 default["memcached"]["listen"] = "0.0.0.0"
 default["memcached"]["port"] = "11211"
+
+# process monitoring
+default["memcached"]["processes"] = [
+  { "name" => "memcached", "shortname" => "memcached" }
+]
