e19a98d285c8c661362703514bad1531c508191e
Wil Reichert <wil.reichert@kt.com>
2013-11-19 14:52:41 +0900

    cleanup

--- metadata.rb
+++ metadata.rb
@@ -10,9 +10,7 @@ version '0.2.4'
   supports os
 end
 
-depends "ktc-utils", "> 0.3.2"
+depends "ktc-utils"
 depends "openstack-common", "~> 0.4.3"
 depends "openstack-object-storage", "~> 1.1.0"
-depends "services", "> 1.1.0"
-depends "sensu"
-depends "ktc-collectd"
+depends "services"
